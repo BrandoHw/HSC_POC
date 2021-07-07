b499dbd7e828461704347d1b6a7b224e2d07e8bb
Brandon Hwong <brandohw@gmail.com>
2021-07-07 13:08:31 +0800

    Remember Me Token

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,6 +29,18 @@
                                 </span>
                             @enderror
                         </div>
+
+                        <div class="form-group">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+                       
+                        </div>
+
                         <div class="d-inline-block w-100">
                             <button type="submit" class="btn btn-primary float-right">Sign in</button>
                         </div>
